79c679e5b6917b0aebcf98273465bcdf84aac792
sruthigopi <101387050+sruthigopi@users.noreply.github.com>
2022-05-05 22:48:27 +0530

    f

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get('/',function(req,res){
 
 
 
-
-app.listen(5000,()=>{
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
     console.log("Server Ready on 5000");  //PART#1 - Point5, Change 3000 to 5000
 });
